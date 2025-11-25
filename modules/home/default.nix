91d52dd7d0072b1364051f5a293f63ec7aec3704
Eugen Meissner <meissner.eugen@gmail.com>
2025-11-25 21:59:29 +0100

    remove impala

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -15,7 +15,6 @@
     ./git.nix                         # version control
     ./gtk.nix                         # gtk theme
     ./hyprland                        # window manager
-    ./impala.nix
     ./nix-search/nix-search.nix       # TUI to search nixpkgs
     ./nvim.nix                        # neovim editor
     ./packages                        # other packages
