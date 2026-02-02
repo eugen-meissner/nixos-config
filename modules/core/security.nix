27a2981b08e05888b6f0a141d9d85cbec4564f86
Eugen Meissner <meissner.eugen@gmail.com>
2026-02-02 20:47:32 +0100

    added yubikey support

--- modules/core/security.nix
+++ modules/core/security.nix
@@ -1,5 +1,13 @@
 { ... }:
 {
+  
+  services.udev.packages = [ pkgs.yubikey-personalization ];
+
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
   security = {
     rtkit.enable = true;
     sudo.enable = true;
