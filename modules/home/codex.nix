ac642867943f928b955553b2cb7baee2e4aec82b
Eugen Meissner <meissner.eugen@gmail.com>
2026-02-20 18:26:17 +0100

    codex flake changes

--- modules/home/codex.nix
+++ modules/home/codex.nix
@@ -1,6 +1,9 @@
 { pkgs, inputs, ... }:
 {
   home.packages = [
-    inputs.codex.packages.${pkgs.system}.default
+    (inputs.codex.packages.${pkgs.system}.default.overrideAttrs (old: {
+      buildInputs = (old.buildInputs or []) ++ [ pkgs.libcap ];
+      nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ pkgs.pkg-config ];
+    }))
   ];
 }
