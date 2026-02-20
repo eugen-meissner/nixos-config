0daeb2149200f01aeb03936d3cb880acebe04790
Eugen Meissner <meissner.eugen@gmail.com>
2026-02-20 11:33:19 +0100

    fix vicinae toggle

--- modules/home/vicinae.nix
+++ modules/home/vicinae.nix
@@ -70,7 +70,34 @@ in
 #    };
   };
 
+  # Theme in both locations for compatibility across versions
   home.file.".local/share/vicinae/themes/custom.toml".text = ''
+    [meta]
+    name = "Gruvbox Dark Hard"
+    description = "Custom Gruvbox-Dark-Hard theme"
+    icon = ""
+    version = 1
+    variant = "dark"
+
+    [colors.core]
+    background = "#1d2021"
+    foreground = "#ebdbb2"
+    secondary_background = "#282828"
+    border = "#928374"
+    accent = "#98971a"
+
+    [colors.accents]
+    blue = "#458588"
+    green = "#98971a"
+    magenta = "#b16286"
+    orange = "#d65d0e"
+    purple = "#b16286"
+    red = "#cc241d"
+    yellow = "#d79921"
+    cyan = "#689d6a"
+  '';
+
+  xdg.configFile."vicinae/themes/custom.toml".text = ''
     [meta]
     name = "Gruvbox Dark Hard"
     description = "Custom Gruvbox-Dark-Hard theme"
